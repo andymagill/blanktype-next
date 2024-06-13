3ae8506625ccd94bbb7e3e305f4fbd714c316eec
Andrew Magill <AndyMagill@Gmail.com>
2024-06-12 22:56:08 -0400

    added embedded SVGs as react components

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'load-svg',
+      enforce: 'pre',
+      transform(_, id) {
+        if (id.endsWith('.svg')) {
+          return 'export default () => {}';
+        }
+      },
+    },
+  ],
   test: {
     environment: "jsdom",
   },
