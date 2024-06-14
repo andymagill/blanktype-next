3f676f25d4e91b462a7e3700f6ad5eff9578d7a7
Andrew Magill <AndyMagill@Gmail.com>
2024-06-14 11:46:49 -0400

    removed CSP headers, part 2

--- next.config.js
+++ next.config.js
@@ -5,19 +5,6 @@ const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
